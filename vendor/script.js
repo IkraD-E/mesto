3215b8d9b35ba57190006159909e5a6a1b7cfd35
Pavel Evtushenko <pasha.512321@gmail.com>
2022-12-25 15:45:20 +0800

    Добавить скрипты для текста в попапе и его разрешение на малых экранах

--- vendor/script.js
+++ vendor/script.js
@@ -4,14 +4,20 @@ let popupCloseBtn = popup.querySelector(".popup__close-btn");
 
 let profileEditButton = document.querySelector(".profile__edit-button");
 
-console.log(popup);
+let nameProfile = document.querySelector(".profile__header");
 
-console.log(profileEditButton);
+let jobProfile = document.querySelector(".profile__text");
 
-console.log(popupCloseBtn);
+let nameInput = document.querySelector(".popup__text_name");
+
+let jobInput = document.querySelector(".popup__text_description");
+
+let formElement = document.querySelector(".popup__submit-btn");
 
 function openPopup() {
     popup.classList.add("popup_opened");
+    nameInput.value = nameProfile.innerHTML; 
+    jobInput.value = jobProfile.innerHTML;
 }
 
 function closePopup() {
@@ -21,3 +27,10 @@ function closePopup() {
 profileEditButton.addEventListener("click", openPopup);
 
 popupCloseBtn.addEventListener("click", closePopup);
+
+formElement.addEventListener("click", function (e) {
+    e.preventDefault();
+    nameProfile.innerHTML = nameInput.value;
+    jobProfile.innerHTML = jobInput.value;
+    popup.classList.remove("popup_opened");
+});
