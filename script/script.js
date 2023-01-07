271500c793030020ffe931a5cac0a4cf457324eb
Pavel Evtushenko <pasha.512321@gmail.com>
2023-01-07 17:56:43 +0800

    Добавить попап для добавления страницы и возможность удалять и добавлять места

--- script/script.js
+++ script/script.js
@@ -1,6 +1,18 @@
 let popup = document.querySelector(".popup");
 
-let popupCloseBtn = popup.querySelector(".popup__close-btn");
+let popupAddPlace = document.querySelector("#popup__add-place");
+
+let profileChange = document.querySelector("#popup__change-profile");
+
+let nameInput = profileChange.querySelector(".popup__text_type_initial");
+
+let jobInput = profileChange.querySelector(".popup__text_type_description");
+
+let placeNameInput = popupAddPlace.querySelector(".popup__text_type_place-name");
+
+let imageSourceInput = popupAddPlace.querySelector(".popup__text_type_image-source");
+
+let profileAdd = document.querySelector(".profile__add-button");
 
 let profileEditButton = document.querySelector(".profile__edit-button");
 
@@ -8,29 +20,114 @@ let nameProfile = document.querySelector(".profile__header");
 
 let jobProfile = document.querySelector(".profile__text");
 
-let nameInput = document.querySelector(".popup__text_type_initial");
 
-let jobInput = document.querySelector(".popup__text_type_description");
+//Создаём массив карт для подгрузки
 
-let formElement = document.querySelector(".popup__submit-btn");
+let elementList = document.querySelector(".elements__list");
 
-function openPopup() {
-    popup.classList.add("popup_opened");
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: './images/Karachaevsck.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: './images/Elbrus.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: './images/Dombai.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: './images/Elbrus.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: './images/Dombai.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: './images/Karachaevsck.jpg'
+    }
+];
+
+// Функция удаления карточки
+function deletePlace(place) {
+    place.target.closest(".element").remove();
+}
+
+function addPlace(placeName, placeImage, position = "start") {
+    const placeTemplate = document.querySelector("#element-template").content;
+    const placeElement = placeTemplate.querySelector(".element").cloneNode(true);
+    const likeBtn = placeElement.querySelector(".element__like-btn")
+    const placeElementPhoto = placeElement.querySelector(".element__photo");
+    const placeElementDelBtn = placeElement.querySelector(".element_delete_button");
+
+    placeElement.querySelector(".element__title").textContent = placeName;
+    placeElementPhoto.src = placeImage;
+    placeElementPhoto.alt = placeName;
+    likeBtn.addEventListener("click", evt => {
+        evt.target.classList.toggle("element__like-btn_active");
+    })
+    placeElementDelBtn.addEventListener("click", evt => deletePlace(evt));
+
+    if (position === "start") {
+        elementList.insertBefore(placeElement, elementList.children[0]);
+    } else {
+        elementList.append(placeElement);
+    }
+
+};
+
+//Добовляем карты на страницу
+initialCards.forEach(item => {
+    addPlace(item["name"], item["link"], "start");
+});
+
+//Работа попапов
+
+function openPopup(name) {
+    name.classList.add("popup_opened");
+}
+
+function getProfileInfo() {
     nameInput.value = nameProfile.innerHTML; 
     jobInput.value = jobProfile.innerHTML;
 }
 
-function closePopup() {
-    popup.classList.remove("popup_opened");
+function closePopup(name) {
+    name.closest(".popup").classList.remove("popup_opened");
 }
 
-profileEditButton.addEventListener("click", openPopup);
-
-popupCloseBtn.addEventListener("click", closePopup);
-
-formElement.addEventListener("click", function (e) {
-    e.preventDefault();
+function submitProfileChanges(evt) {
+    evt.preventDefault();
+    console.log(evt.target.closest(".popup").id)
     nameProfile.innerHTML = nameInput.value;
     jobProfile.innerHTML = jobInput.value;
-    closePopup();
+    closePopup(evt.target);
+}
+
+profileEditButton.addEventListener("click",  () => {
+    const popupCloseBtn = profileChange.querySelector(".popup__close-btn");
+    const popupSubmitBtn = profileChange.querySelector(".popup__submit-btn");
+    popupSubmitBtn.addEventListener("click",  evt => submitProfileChanges(evt));
+    openPopup(profileChange);
+    getProfileInfo();
+    popupCloseBtn.addEventListener("click",  () => closePopup(profileChange));
 });
+
+profileAdd.addEventListener("click",  () => {
+    const popupCloseBtn = popupAddPlace.querySelector(".popup__close-btn");
+    const popupSubmitBtn = popupAddPlace.querySelector(".popup__submit-btn");
+    openPopup(popupAddPlace);
+    popupSubmitBtn.addEventListener("click",  evt => {
+        evt.preventDefault();
+        const place = placeNameInput.value;
+        const image = imageSourceInput.value;
+        addPlace(place, image)
+        closePopup(evt.target);
+    });
+    popupCloseBtn.addEventListener("click",  () => closePopup(popupAddPlace));
+});
+
