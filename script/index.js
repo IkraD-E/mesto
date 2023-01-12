5b7641850dec34f06f0e2f004517deb2bf04e585
Pavel Evtushenko <pasha.512321@gmail.com>
2023-01-13 00:47:25 +0800

    Прибраться в строении JS кода страницы

--- script/index.js
+++ script/index.js
@@ -29,58 +29,32 @@ const placeTemplate = document.querySelector("#element-template").content;
 
 const popupAddPlaceForm = popupAddPlace.querySelector(".popup__form");
 
-// Image
-
 const popupImageCloseBtn = popupImage.querySelector(".popup__close-btn");
-
-popupImageCloseBtn.addEventListener("click",  () => closePopup(popupImage));
-
-// ProfileChange
 
 const popupEditCloseBtn = profileChange.querySelector(".popup__close-btn");
 
-function submitProfileChanges(event) {
+const popupAddPlaceCloseBtn = popupAddPlace.querySelector(".popup__close-btn");
+
+const elementPhoto = popupImage.querySelector(".popup__image");
+
+const elementPhotoHeader = popupImage.querySelector(".popup__image-header");
+
+function handleSubmitProfileChanges(event) {
     event.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     closePopup(profileChange);
 }
 
-popupEditForm.addEventListener("submit",  submitProfileChanges);
-
-popupEditCloseBtn.addEventListener("click",  () => closePopup(profileChange));
-
-// AddPlace
-
-const popupAddPlaceCloseBtn = popupAddPlace.querySelector(".popup__close-btn");
-
-popupAddPlaceCloseBtn.addEventListener("click",  () => closePopup(popupAddPlace));
-
-popupAddPlaceForm.addEventListener("submit",  event => {
-    event.preventDefault();
-    const place = placeNameInput.value;
-    const image = imageSourceInput.value;
-    renderCard(addPlace(place, image));
-    closePopup(popupAddPlace);
-    event.target.reset(); 
-});
-
-function deletePlace(event) {
-    event.target.closest(".element").remove();
-}
-
-
-//Добовляем карты на страницу
-
-function renderCard(cardElement, position = "start") {
-    if (position === "start") {
+function renderCard(cardElement, isStart) {
+    if (isStart) {
         elementList.prepend(cardElement);
     } else {
         elementList.append(cardElement);
     }
 }
 
-function addPlace(placeName, placeImage) {
+function createPlace(placeName, placeImage) {
     const placeElement = placeTemplate.querySelector(".element").cloneNode(true);
     const likeBtn = placeElement.querySelector(".element__like-btn");
     const placeElementPhoto = placeElement.querySelector(".element__photo");
@@ -93,12 +67,9 @@ function addPlace(placeName, placeImage) {
         evt.target.classList.toggle("element__like-btn_active");
     });
 
-    placeElementDelBtn.addEventListener("click", deletePlace);
+    placeElementDelBtn.addEventListener("click", handleDeletePlace);
 
     placeElementPhoto.addEventListener("click", () => {
-        const elementPhoto = popupImage.querySelector(".popup__image");
-        const elementPhotoHeader = popupImage.querySelector(".popup__image-header");
-
         elementPhoto.src = placeElementPhoto.src;
         elementPhoto.alt = placeName;
         elementPhotoHeader.textContent = placeName;
@@ -108,13 +79,13 @@ function addPlace(placeName, placeImage) {
     return(placeElement);
 }
 
-initialCards.forEach(item => renderCard(addPlace(item["name"], item["link"]), null));
+function handleDeletePlace(event) {
+    event.target.closest(".element").remove();
+}
 
 function openPopup(popup) {
     popup.classList.add("popup_opened");
 }
-
-//Работа попапов
 
 function getProfileInfo() {
     nameInput.value = nameProfile.textContent; 
@@ -133,3 +104,22 @@ profileEditButton.addEventListener("click",  () => {
 profileAdd.addEventListener("click",  () => {
     openPopup(popupAddPlace);
 });
+
+popupImageCloseBtn.addEventListener("click",  () => closePopup(popupImage));
+
+popupEditForm.addEventListener("submit",  handleSubmitProfileChanges);
+
+popupEditCloseBtn.addEventListener("click",  () => closePopup(profileChange));
+
+popupAddPlaceCloseBtn.addEventListener("click",  () => closePopup(popupAddPlace));
+
+popupAddPlaceForm.addEventListener("submit",  event => {
+    event.preventDefault();
+    const place = placeNameInput.value;
+    const image = imageSourceInput.value;
+    renderCard(createPlace(place, image), true);
+    closePopup(popupAddPlace);
+    event.target.reset(); 
+});
+
+initialCards.forEach(item => renderCard(createPlace(item["name"], item["link"])));
