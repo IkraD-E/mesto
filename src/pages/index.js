8cfeebdbec70d6a13a9912a64533080c1e9076fd
Pavel Evtushenko <pasha.512321@gmail.com>
2023-03-29 16:19:37 +0800

    Сделать запросы к карточкам сервера для добавления их на сайт и создания новых карточек

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import Card from "../script/components/Card.js";
 
 import FormValidator from "../script/components/FormValidator.js";
 
-import { initialCards } from "../script/cards.js";
-
 import { Section } from "../script/components/Section.js";
 
 import {PopupWithImage} from "../script/components/PopupWithImage.js";
@@ -23,6 +21,18 @@ import {
     validationList,
     addButton
 } from '../script/utils/constants.js';
+
+//Работа с сервером
+//Информация о пользователе
+function getUserInfo() {
+    return fetch('https://mesto.nomoreparties.co/v1/cohort-62/users/me ', {
+        headers: {
+            authorization: 'e055b3b1-f0a3-420f-954c-707ea8c5fb7b'
+        }
+    })
+        .then(res => res.json())
+        .catch(res => console.log(`Ошибка: ${res.status}`))
+}
 
 const handleSubmitProfileChanges = () => {
     userInfo.setNewUserInfo(popupProfile.returnInputValues());
@@ -69,7 +79,7 @@ const enableValidation = (validationList) => {
 
 enableValidation(validationList);
 
-// Создание стандартного набора карточек
+// Создание класса для добавления новых мест с  его настройкой
 
 function createCard(element) {
     const card = new Card(element, '#element-template', handleCardClick);
@@ -80,23 +90,69 @@ function createCard(element) {
 
 }
 
-const cardList = new Section({ items:initialCards, 
+const cardList = new Section({ items: null, 
         renderer: createCard
     }, 
     '.elements__list');
 
-cardList.createCard();
+//Создаём карточки с из информации с сервера
+
+function createCardFromServer() {
+    fetch('https://mesto.nomoreparties.co/v1/cohort-62/cards', {
+        headers: {
+            authorization: 'e055b3b1-f0a3-420f-954c-707ea8c5fb7b'
+        }
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            }
+            return Promise.reject(res.status)
+        })
+        .then(cards => {
+            cardList.createCardList(cards);
+        })
+        .catch(err => console.log(`Ошибка: ${err}`))
+}
+
+createCardFromServer();
 
 // Логика добавления карточек новых мест
 
+function addNewPlaceToSerwer(placeName, placeLink) {
+    console.log(`Вот информация ${placeName}, ${placeLink}`);
+    fetch('https://mesto.nomoreparties.co/v1/cohort-62/cards', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: placeName,
+            link: placeLink
+        }),
+        headers: {
+            'authorization': 'e055b3b1-f0a3-420f-954c-707ea8c5fb7b',
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            }
+            return Promise.reject(res.status)
+        })
+        .catch(err => console.log(`Ошибка: ${err}`))
+}
+
 const addNewPlace = () => {
     const element = popupPlace.returnInputValues();
+
+    addNewPlaceToSerwer(element.name, element.link)
     
-    cardList.addItem(element);
+    cardList.addItem(element, true);
     popupPlace.close();
 
     addButton.focus()
 }
+
+//Включаем Попапы добавления нового места и изменения профиля
 
 const popupPlace = new PopupWithForm(addNewPlace,"#popup__add-place");
 
--- src/script/components/Section.js
+++ src/script/components/Section.js
@@ -11,7 +11,17 @@ export class Section{
         });
     }
 
-    addItem(element){
-        this._container.prepend(this._renderer(element));
+    createCardList(cardList){
+        cardList.forEach(card =>{
+            this.addItem(card);
+        })
+    }
+
+    addItem(element, positionStart){
+        if (positionStart) {
+            this._container.prepend(this._renderer(element));
+        } else {
+            this._container.append(this._renderer(element));
+        }
     }
 }
